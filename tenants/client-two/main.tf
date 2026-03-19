034a0784d16645174491a150dab09908e08ff922
David Vestal <vestal.d@gmail.com>
2026-03-19 13:19:53 -0500

    upgrading

--- tenants/client-two/main.tf
+++ tenants/client-two/main.tf
@@ -1,7 +1,7 @@
 data "google_project" "current" {}
 
 module "poseidon" {
-  source = "../../applications/poseidon/v0.0.1"
+  source = "../../applications/poseidon/v0.0.2"
 
   project = data.google_project.current.project_id
 }
