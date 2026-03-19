7c80c0213db9c27456b892a87c1d00758fb4ed9c
David Vestal <vestal.d@gmail.com>
2026-03-18 19:54:23 -0500

    upgrade client-one and the template to poseidon version to 0.0.2

--- template/main.tf
+++ template/main.tf
@@ -1,8 +1,13 @@
 data "google_project" "current" {}
 
 module "poseidon" {
-  source = "git::https://github.com/dvestal/vplatform-apps.git//poseidon?ref=v0.0.1"
+  source = "git::https://github.com/dvestal/vplatform.git//applications/poseidon/v0.0.2"
 
-  project  = data.google_project.current.project_id
-  location = "us-east1"
+  project = data.google_project.current.project_id
+}
+
+module "zeus" {
+  source = "git::https://github.com/dvestal/vplatform.git//applications/zeus/v0.0.1"
+
+  project = data.google_project.current.project_id
 }
