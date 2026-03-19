6a90ec955c1ad1b61793d90c30c04d7aaadabb73
David Vestal <vestal.d@gmail.com>
2026-03-19 12:01:04 -0500

    removing bootstrap for clarity

--- bootstrap/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "repository_id" {
-  description = "Artifact Registry repository id"
-  value       = google_artifact_registry_repository.images.repository_id
-}
-
-output "repository_url" {
-  description = "Full OCI pull/push URL for the repository (REGION-docker.pkg.dev/PROJECT/REPO)"
-  value       = "${var.region}-docker.pkg.dev/${var.project}/${google_artifact_registry_repository.images.repository_id}"
-}
