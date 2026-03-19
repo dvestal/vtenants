5dd0989198bf75b8a5843102b2259142b597a7e0
David Vestal <vestal.d@gmail.com>
2026-03-18 19:37:55 -0500

    use the directory versioned workflow

--- client-one/main.tf
+++ client-one/main.tf
@@ -1,8 +1,7 @@
 data "google_project" "current" {}
 
 module "poseidon" {
-  source = "git::https://github.com/dvestal/vplatform-apps.git//poseidon?ref=v0.0.1"
+  source = "git::https://github.com/dvestal/vplatform.git//applications/poseidon/v0.0.1"
 
-  project  = data.google_project.current.project_id
-  location = "us-east1"
+  project = data.google_project.current.project_id
 }
